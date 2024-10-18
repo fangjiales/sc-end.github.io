52a2a72b8139905cd3d558185992ce006915c0f9
f_lesky <2451283865@qq.com>
2024-10-18 13:34:32 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,4 +20,4 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:fangjiales/sc-end.github.io.git main:gh-pages
+git push -f git@github.com:fangjiales/sc-end.github.io.git master:gh-pages
